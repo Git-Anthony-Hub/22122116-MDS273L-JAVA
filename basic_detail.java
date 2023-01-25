b39f2cec11d55daa758b34e4159543e0a48de1ae
Git-Anthony-Hub <christian.anthony@msds.christuniversity.in>
2023-01-25 12:27:55 +0530

    Lab 1

--- basic_detail.java
+++ basic_detail.java
@@ -19,61 +19,146 @@ public class basic_detail{
             System.out.println("Female");
         }
 
-        String[] north = {"Delhi",
-            "Haryana",
-            "Jammu and Kashmir",
-            "Himachal Pradesh",
-            "Uttarpradesh",
-            "Punjab",
-            "Uttarakhand",
-            "Rajasthan",
-            "Ladakh",
-            "Chandigarh"};
+            String word = "";
 
-        String[] south = {"Andaman & Nicobar",
+            switch(State){
+                case "Delhi":
+                word = "The Student is from the southern states of India";
+                break;
 
-            "Andhra Pradesh",
-            
-            "Karnataka",
-            
-            "Kerala",
-            
-            "Lakshadweep",
-            
-            "Pondicherry",
-            
-            "Tamilnadu"};
+                case "Haryana":
+                word = "The Student is from the southern states of India";
+                break;
 
-        String ans = "";
-        String zone = "";
-        do{
-            for(int i = 0; i<north.length; i++){
-                if (State == north[i]){
-                    zone = "north";
-                    ans = "done";
-                    break;
-                }
-            };
+                case "Jammu and Kashmir":
+                word = "The Student is from the southern states of India";
+                break;
 
-            for(int i = 0; i<south.length; i++){
-                if (State == south[i]){
-                    zone = "south";
-                    ans = "done";
-                    break;
-                }
+                case "Himachal Pradesh":
+                word = "The Student is from the southern states of India";
+                break;
+
+                case "Uttarpradesh":
+                word = "The Student is from the southern states of India";
+                break;
+
+                case "Punjab":
+                word = "The Student is from the southern states of India";
+                break;
+
+                case "Uttarakhand":
+                word = "The Student is from the southern states of India";
+                break;
+
+                case "Rajasthan":
+                word = "The Student is from the southern states of India";
+                break;
+
+                case "Ladakh":
+                word = "The Student is from the southern states of India";
+                break;
+
+                case "Chandigarh":
+                word = "The Student is from the southern states of India";
+                break;
+
+                
+                case "Andaman & Nicobar":
+                word = "The Student is from the northern states of India";
+                break;
+
+                case "Andhra Pradesh":
+                word = "The Student is from the northern states of India";
+                break;
+
+                case "Karnataka":
+                word = "The Student is from the northern states of India";
+                break;
+                
+                case "Lakshadweep":
+                word = "The Student is from the northern states of India";
+                break;
+
+                case "Pondicherry":
+                word = "The Student is from the northern states of India";
+                break;
+
+                case "Tamilnadu":
+                word = "The Student is from the northern states of India";
+                break;
+
+
+                case "Bihar":
+                word = "The Student is from the eastern states of India";
+                break;
+                
+                case "West Bengal":
+                word = "The Student is from the eastern states of India";
+                break;
+                case "Jharkhand":
+                word = "The Student is from the eastern states of India";
+                break;
+                case "Orissa":
+                word = "The Student is from the eastern states of India";
+                break;
+
+                case "Dadra & Nagar Haveli":
+                word = "The Student is from the eastern states of India";
+                break;
+                case "Daman & Diu":
+                word = "The Student is from the eastern states of India";
+                break;
+                case "Goa":
+                word = "The Student is from the eastern states of India";
+                break;
+                case "Gujarat":
+                word = "The Student is from the eastern states of India";
+                break;
+                case "Maharashtra":
+                word = "The Student is from the eastern states of India";
+                break;
+                
+                case "Rajashthan":
+                word = "The Student is from the eastern states of India";
+                break;
+
             }
-        }while(ans != "done");
 
-        String direction;
-        switch(zone){
-            case "north":
-            direction = "The Student is from the northern states of India";
-            break;
+            System.out.println(word)
 
-            case "south":
-            direction = "The Student is from the southern states of India";
-            break;
-        }
+
+
+
+        // String ans = "";
+        // String zone = "";
+        // do{
+        //     for(int i = 0; i<north.length; i++){
+        //         if (State == north[i]){
+        //             zone = "north";
+        //             ans = "done";
+        //             break;
+        //         }
+        //     };
+
+        //     for(int i = 0; i<south.length; i++){
+        //         if (State == south[i]){
+        //             zone = "south";
+        //             ans = "done";
+        //             break;
+        //         }
+        //     }
+        // }while(ans != "done");
+
+        // String direction;
+        // switch(zone){
+        //     case "north":
+        //     direction = "The Student is from the northern states of India";
+        //     break;
+
+        //     case "south":
+        //     direction = "The Student is from the southern states of India";
+        //     break;
+        // }
 
     }
 }
